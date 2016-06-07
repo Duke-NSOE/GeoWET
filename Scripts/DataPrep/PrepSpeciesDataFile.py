10a265fd9797d589d99dfa33a63c9bf6a8609b8c
John Fay <John.Fay@duke.edu>
2016-06-07 15:52:42 +1000

    Adds working version of PrepSpeciesData script

--- Scripts/DataPrep/PrepSpeciesDataFile.py
+++ Scripts/DataPrep/PrepSpeciesDataFile.py
@@ -16,15 +16,13 @@ import pandas as pd
 from scipy import stats
 
 #Get the species file
-sppName = 'Acantharchus_pomotis'
+sppName = 'Nocomis_leptocephalus'
 sppFN = r'C:\workspace\GeoWET\Data\SpeciesModels\{}.csv'.format(sppName)
+outFN = r'C:\workspace\GeoWET\Data\SpeciesModels\{}_swd.csv'.format(sppName)
 
 #Read in file as pandas dataframe
-if not ("initialDF" in dir()):
-    print "Reading data for {}".format(sppName)
-    initialDF = pd.read_csv(sppFN)
-    
-sppDF = initialDF #pd.read_csv(sppFN)
+print "Reading data for {}".format(sppName)
+sppDF = pd.read_csv(sppFN)
 
 def removeAbsenceNullRows(theDF):
     '''Remove ROWS where spp is absence and attribute has -9999'''
@@ -36,8 +34,8 @@ def removeAbsenceNullRows(theDF):
         #Calculate the number of records deleted
         dbDelta = dbSize - len(theDF)
         #LOGGING: Report how many records deleted for the current field
-        if dbDelta > 0:
-            print "...{} records removed via {} field".format(dbDelta,fld)
+        #if dbDelta > 0:
+            #print "...{} records removed via {} field".format(dbDelta,fld)
     return theDF
 
 def removePresenceNullColumns(theDF):
@@ -46,41 +44,71 @@ def removePresenceNullColumns(theDF):
         #Get the min value of the field
         if theDF[fld].min() == -9999:
             #Drop the fields
-            print "...Removing: {}".format(fld)
+            #print "...Removing: {}".format(fld)
             theDF.drop(fld,axis=1,inplace=True)
     return theDF
 
 def removeUncorrelated(theDF):
     '''Remove attributes with no correlation with presence/absence'''
-    theDF = sppDF3
-    threshold = 0.7
     sppVector = theDF[sppName]
-    for fld in list(theDF.columns)[7:]:
+    for fld in list(theDF.columns)[6:]:
         envVector = theDF[fld]
         pearson = stats.pearsonr(sppVector, envVector)
         coeff = pearson[0]
         pValue = pearson[1]
         #Print output to the CSV file
         if abs(pValue) > 0.05:
-            print "%s is not significant (p = %2.3f)" %(fld,pValue)
+            #print "%s is not significant (p = %2.3f)" %(fld,pValue)
             theDF.drop(fld,axis=1,inplace=True)
+    return theDF
+
+def removeXCorrelated(theDF,threshold=0.75):
+    #Initialize the list of fields to drop
+    dropFlds = []
+    #Include only StreamCat attributes
+    flds = list(theDF.columns)[6:]
+    nCols = len(flds)
+    #Compute cross correlation pairs; add 2nd field to drop list
+    for i in range(0,nCols):
+        for j in range(0,nCols):
+            if j > i:
+                iFld = flds[i]; jFld =flds[j]
+                v1 = theDF[iFld]
+                v2 = theDF[jFld]
+                # Calculate the correlation coefficient
+                pearson = stats.pearsonr(v1, v2)[0]
+                # If the coefficient is > the threshold consider them redundant and add to the CSV
+                if abs(pearson) >= float(threshold):
+                    #print "{}, {}, {}".format(iFld,jFld,pearson)
+                    #Need ranking to decide which field to drop
+                    #For now, just drop the 2nd field
+                    if not (jFld in dropFlds): dropFlds.append(jFld)
+    #Drop cross correlated fields
+    for dropFld in dropFlds:
+        theDF.drop(dropFld,axis=1,inplace=True)
+    #Return the data frame
     return theDF
 
 #-------------------------------------------------------------            
 print "Removing absence records with no catchment data"
 print "...Starting with {} records".format(len(sppDF))
-#sppDF2 = removeAbsenceNullRows(sppDF)
-print "...{} records remain".format(len(sppDF2))
+sppDF = removeAbsenceNullRows(sppDF)
+print "...{} records remain".format(len(sppDF))
 
 print "Removing attributes with null values"
-print "...Starting with {} columns".format(sppDF2.shape[1])
-#sppDF3 = removePresenceNullColumns(sppDF2)
-print "...{} columns remain".format(sppDF3.shape[1])
+print "...Starting with {} columns".format(sppDF.shape[1])
+sppDF = removePresenceNullColumns(sppDF)
+print "...{} columns remain".format(sppDF.shape[1])
 
 print "Removing attributes not correlated with presence absence"
-print "...Starting with {} columns".format(sppDF3.shape[1])
-sppDF4 = removeUncorrelated(sppDF3)
-print "...{} columns remain".format(sppDF3.shape[1])
+print "...Starting with {} columns".format(sppDF.shape[1])
+sppDF = removeUncorrelated(sppDF)
+print "...{} columns remain".format(sppDF.shape[1])
 
+print "Removing cross correlated attributes"
+print "...Starting with {} columns".format(sppDF.shape[1])
+sppDF = removeXCorrelated(sppDF)
+print "...{} columns remain".format(sppDF.shape[1])
 
-   
+#write file to csv
+sppDF.to_csv(outFN,index_label="OID")
