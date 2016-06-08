973d9b208fa8211ea728579b261a1dbc85a64d62
John Fay <John.Fay@duke.edu>
2016-06-08 14:41:55 +1000

    Sets output to include spp name/background column
    Also checks that the species name occurs in the species CSV file

--- Scripts/DataPrep/ExtractSpeciesData.py
+++ Scripts/DataPrep/ExtractSpeciesData.py
@@ -1,10 +1,22 @@
 #ExtractSpeciesData.py
 #
-# Summary: Extracts catchment data for HUC8s in which species occurs.
+# Summary: Extracts catchment data for HUC8s in which species occurs and
+#  merges the data with species presence absences records.
 #
-# Workflow: Pulls catchment records where the species occurs and creates
-#  a list of the unique HUC8s from these records. Then extracts catchment
-#  data for these HUC8s and writes them all to a table
+# Usage: ExtractSpeciesData(Species Name,
+#                           Species Occurrences,
+#                           Stream Cat data folder,
+#                           Output CSV filename)
+#
+# Workflow:
+#  - Checks that the supplied species name occurs in the species CSV file.
+#  - Creates a list of the HUC8s in which the species has been observed.
+#  - Extracts catchment records from each of the Stream Cat CSV file occurring
+#    within these HUC8s into a series of pandas data frames.
+#  - Merges each individual Stream Cat CSV file into a single pandas dataframe.
+#  - Prepends a column of species presence and absence (background) to the
+#    merged data frame
+#  - Writes the contents out to a CSV file.
 #
 # June 2016
 # John.Fay@duke.edu
@@ -14,8 +26,6 @@ import pandas as pd
 import numpy as np
 
 #Species to process
-#spp = "Etheostoma_olmstedi"
-#spp = "Acantharchus_pomotis"
 spp = "Nocomis_leptocephalus"
 
 #Workspaces
@@ -23,14 +33,28 @@ eoCSV = r'C:\workspace\GeoWET\Data\ToolData\SpeciesOccurrences.csv'
 dataFldr = r'C:\workspace\GeoWET\Data\StreamCat\AllRegions'
 outFN = r'C:\workspace\GeoWET\Data\SpeciesModels\{}.csv'.format(spp)
 
-##Functions##
+##--------Functions--------##
+def checkSpeciesName(speciesName,eoCSV):
+    '''Ensures that the supplied species name exists in the observation table'''
+    import csv
+    fileObj = open(eoCSV,'rt')
+    csvReader = csv.reader(fileObj)
+    fldNames = csvReader.next()
+    fileObj.close()
+    if not speciesName in fldNames:
+        print "***{}*** does not occur in the \n   {} file".format(speciesName,eoCSV)
+        print "   Check your species name and try again..."
+        sys.exit(1)
+    else:
+        return      
+
 def getHUC8s(eoCSV,speciesName):
     '''Returns a list of the HUC8s in which a species occurs'''
     #Create a data frame from the species data
-    useCols = ["REACHCODE",spp]
+    useCols = ["REACHCODE",speciesName]
     eoDF = pd.read_csv(eoCSV,usecols=useCols,dtype={'REACHCODE':str})
     #Pull just the records where the species occurs
-    sppDF = eoDF[eoDF[spp] == 1]
+    sppDF = eoDF[eoDF[speciesName] == 1]
     #Get a list of the huc8s in which the spp is found
     huc8s = sppDF['REACHCODE'].str[:8].unique()
     #Return the list
@@ -55,9 +79,23 @@ def mergePresAbs(eoCSV,speciesName,dataDF):
     #Join the presence absence data to the catchment data frame
     outDF = pd.merge(eoDF,dataDF,how='right',left_on="FEATUREID",right_on="FEATUREID")
 
-    #Change NaNs to 0
+    #Add the species field and set default to "background"
+    outDF.insert(0,"Species","Background")
+
+    #Change NaNs to "Background
     outDF[speciesName] = outDF[speciesName].fillna(0)
+
+    #Change Species values in records where observed
+    outDF.loc[outDF[speciesName] == 1, 'Species'] = speciesName
+
+    #Drop the speciesName column
+    outDF.drop(speciesName,axis=1,inplace=True)
+    
     return outDF
+
+##------Procedures--------
+#Make sure the species name is valid
+checkSpeciesName(spp,eoCSV)
 
 #Create a list of HUC8s in which the species was observed
 huc8s = getHUC8s(eoCSV, spp)
