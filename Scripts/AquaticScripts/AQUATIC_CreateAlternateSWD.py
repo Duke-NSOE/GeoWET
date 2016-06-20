4def64bb4b34c8230d982aef8ecec49dcf95e611
John Fay <John.Fay@duke.edu>
2016-06-20 16:54:25 +1000

    Major code revision

--- Scripts/AquaticScripts/AQUATIC_CreateAlternateSWD.py
+++ Scripts/AquaticScripts/AQUATIC_CreateAlternateSWD.py
@@ -41,6 +41,8 @@ flowlineFC = r'C:\workspace\GeoWET\Data\EEP_030501.gdb\NHDFlowlines'
 elevRaster = r'C:\workspace\GeoWET\Data\EEP_030501.gdb\elev_cm'
 catchmentFC = r'C:\workspace\GeoWET\Data\EEP_030501.gdb\NHDCatchments'
 huc12FC = r'C:\workspace\GeoWET\Data\EEP_030501.gdb\HUC12'
+flowNet = r'C:\workspace\GeoWET\Data\ToolData\NHD_H_03050102_GDB.gdb\Hydrography\HYDRO_NET'
+flowNetFC = os.path.join(flowNet,'..',"NHDFlowline")
 
 ##-----------FUNCTIONS----------
 def getGridcodes(prjFC,catchFC):
@@ -52,12 +54,33 @@ def getGridcodes(prjFC,catchFC):
     #Loop through the feature classes and add them to a list
     with arcpy.da.SearchCursor(clipFC,"GRIDCODE") as cur:
         for rec in cur:
-            gridcodes.append(str(rec[0]))
+            gridcodes.append(rec[0])
     #Delete the feature class
     arcpy.Delete_management(clipFC)
     #Return the list of reachcodes
     return gridcodes
 
+def getDownstreamGridCodes(gridcode,catchFC,flownet,flownetFC):
+    '''Traces downstream from the project's catchment and lists all downstream catchment GRIDCODES'''
+    #Get the catchment corresponding to the gridcode
+    theCatchLyr = arcpy.MakeFeatureLayer_management(catchFC,"theCatch","GRIDCODE = {}".format(gridcode))
+    #Clip the flowine within the catchment and get its lowest point
+    theFlowline = arcpy.Clip_analysis(flownetFC,theCatchLyr,"in_memory/theFlowline")
+    theFlowpoint = arcpy.FeatureVerticesToPoints_management(theFlowline,"in_memory/thePoint","END")
+    #Trace from this point downstream to the end of the HUC8 geometric network
+    theTraceLyr = arcpy.TraceGeometricNetwork_management(flownet,"DownStrmLyr",theFlowpoint,"TRACE_DOWNSTREAM")
+    #Extract the line feature
+    theTraceLine = arcpy.SelectData_management(theTraceLyr,"NHDFlowline")
+    #Make a new feature layer of catchments and select those that intersect the downstream trace
+    theCatchLyr = arcpy.MakeFeatureLayer_management(catchFC,"theCatch")
+    theCatchments = arcpy.SelectLayerByLocation_management(theCatchLyr,"INTERSECT",theTraceLine)
+    #Create a list of catchment GRIDCODEs in the selected catchments
+    outGridcodes = []
+    with arcpy.da.SearchCursor(theCatchments,"GRIDCODE") as cur:
+        for rec in cur:
+            outGridcodes.append(rec[0])
+    return outGridcodes
+   
 def getHUC8(prjFC,hucFC):
     '''Returns the HUC8s in which the project occurs (via clip)'''
     #Create a feature class of just the catchments intersecting the project
@@ -71,14 +94,48 @@ def getHUC8(prjFC,hucFC):
     #Return the list of reachcodes
     return huc8
 
-def getHUC8Data(csvFile,huc8):
+def getCatchmentData(csvFile,downstreamCodes):
     '''Creates a dataframe of the HUC8 Records from a catchment attribute file'''
     #Convert CSV to pandas data frame
-    dtypes = {"GRIDCODE":np.str,"FEATUREID":np.str,"REACHCODE":np.str,"HUC_12":np.str} 
+    dtypes = {"GRIDCODE":np.long,"FEATUREID":np.str,"REACHCODE":np.str,"HUC_12":np.str} 
     fullDF = pd.read_csv(csvFile,dtype=dtypes)
     #Subset records
-    selectDF = fullDF[fullDF["REACHCODE"].str[:8] == huc8]
+    selectDF = fullDF[fullDF["GRIDCODE"].isin(downstreamCodes)]
     return selectDF
+
+def makeDataFramefromCSVs(dataFldr,gridcodes):
+    '''Returns a dataframe of stream cat attributes for selected gridcodes'''
+    #Initialize the list of dataframes to merge
+    dataFrames = []
+    #Get a listing of all the files in the folder
+    dataFiles = os.listdir(dataFldr)
+    #Loop through each file
+    for dataFile in dataFiles:
+        #Skip if not a CSV file
+        if dataFile[-4:].upper() <> ".CSV": continue
+        print "Processing streamcat file: {}".format(dataFile)
+        #Get the full file path
+        csvFN = os.path.join(dataFolder,dataFile)
+        #Convert catchment records to a data frame
+        csvDF = getCatchmentData(csvFN,gridcodes)
+        #Remove duplicate column names (unless its the first file)
+        if len(dataFrames) == 0:
+            colNames = list(csvDF.columns)
+        else:
+            newCols = []
+            for col in list(csvDF.columns):
+                if not (col in colNames):
+                    newCols.append(col)
+                    colNames.append(col)
+            csvDF = csvDF[newCols]
+        #Append to dataDFs
+        dataFrames.append(csvDF)
+
+    #Merge dataframes
+    print("...merging dataframes")
+    dataDF = pd.concat(dataFrames,axis=1)
+    #Return the data frame
+    return dataDF
 
 def getRasterCounts(rstr):
     '''Returns a dictionary of nlcd codes (keys) and area (values)'''
@@ -106,7 +163,10 @@ def getNLCDValues(flwlineFC,nlcdRstr,elevRstr,prjFC):
     #Get the NLCD raster
     selectNLCD = arcpy.sa.ExtractByMask(nlcdRstr,prjFC)
     nlcdDict = getRasterCounts(selectNLCD)
-    arcpy.env.extent =selectNLCD
+    
+    #Get the current extent (to reset later) and set to project
+    initExtent = arcpy.env.extent
+    arcpy.env.extent = selectNLCD
     
     #Create a riparian buffer mask and NLCD raster
     bufMask = arcpy.sa.EucDistance(flwlineFC,100)
@@ -134,6 +194,9 @@ def getNLCDValues(flwlineFC,nlcdRstr,elevRstr,prjFC):
             else:
                 valList.append(0)
         outDict[k] = valList
+        
+    #Reset the extent
+    arcpy.env.extent = initExtent
 
     #Return the series
     return outDict
@@ -161,100 +224,122 @@ def updateRasterCounts(datadict,prjType="Wetland"):
     return datadict
 
 ##---PROCEDURES----
+##[1]Create a dataframe of NLCD areas lost or gained
 #Create a dictionary of NLCD changes in the catchment
 dataDict = getNLCDValues(flowlineFC,nlcdRaster,elevRaster,projectFC)
-
 #Update the dataDict to show gain in wetland/forest
 dataDict = updateRasterCounts(dataDict,projectType)
-   
-#Get the list of NHD gridcodes within the project
-###>>>NEED TO ADAPT TO IF A PROJECT SPANS CATCHMENTS<<<<
+
+##[2]Get the GRIDCODEs of the catchment(s) in which the project occurs
 gridCodes = getGridcodes(projectFC,catchmentFC)
 gridCode = gridCodes[0]
+print "Project occurs in catchment {}".format(gridCode)
 
-#Get the HUC8 in which the project occurs
-huc8 = getHUC8(projectFC,huc12FC)
-print "Project is in HUC8: {}".format(huc8)
+##[3]Trace downstream to get the downstream catchments
+dsGridCodes = getDownstreamGridCodes(gridCode,catchmentFC,flowNet,flowNetFC)
+print "{} catchments downstream".format(len(dsGridCodes))
 
-#Import the Excel file listing the fields to change...
+##[4]Create dataframes of selected catchments and merge into one...
+#Append the project catchment to downstream ones
+gridCodes = dsGridCodes
+gridCodes.append(gridCode)
+dataDF = makeDataFramefromCSVs(dataFolder,gridCodes)
+print "{} catchment attributes extracted".format(dataDF.shape[1])
+
+##[5]Adjust NLCD related attributes in the project catchment
+#Read in the StreamCatInfo table
 print "Reading in field mappings"
-fldmapDF = pd.read_excel(fieldMapXLS,'StreamCatInfo')
+lutDF = pd.read_excel(fieldMapXLS,'StreamCatInfo')
 
-#Get a list of all files
-allCSVFiles = pd.unique(fldmapDF.File).tolist()
+#Process changes record for the catchment itself
+catchDF = dataDF[dataDF.GRIDCODE == gridCode]
+#Get the catchment area values
+catchArea = catchDF.CatAreaSqKm.values[0]
+riparianArea = catchDF.CatAreaSqKmRp100.values[0]
+midslopeArea = catchDF.PctAg2006Slp10Ws.values[0]
+highslopeArea = catchDF.PctAg2006Slp20Ws.values[0]
+#Put area values into a list
+baseAreas = (catchArea,riparianArea,midslopeArea,highslopeArea)
 
-#Filter for fields changed in wetland project
-fldsDF = fldmapDF[fldmapDF["NLCDMap"] > -9999]
-modCSVFiles = pd.unique(fldsDF.File).tolist()
+#Reduce field map dataframe to only catchment (not watershed) records
+catOnlyDF = lutDF.loc[lutDF.CatWs == 'CAT']
 
-#Create list of dataframes
-dataFrames = []
+#Loop through the land cover types that have changed for the current catchment
+for nlcdType, changeValues in dataDict.items():
+    ##Process catchment wide values
+    #Extract fields mapped to the current land cover type
+    nlcdDF = catOnlyDF.loc[(catOnlyDF.NLCDMap==nlcdType)]
+    #Loop through retrieved attributes 
+    for nlcdAttrib in nlcdDF.Attribute.values:
+        #Determine whether it's full catchment, riparian, or mid/high slope; set the index
+        if "Rp" in nlcdAttrib: idx = 1        # Riparian
+        elif "Slp10" in nlcdAttrib: idx = 2   # Mid-slope
+        elif "Slp20" in nlcdAttrib: idx = 3   # High-slope
+        else: idx = 0                         # Catchment wide
+        #Get the appropriate change area
+        changeArea = changeValues[idx]
+        #Skip if no change has occurred
+        if changeArea == 0: continue
+        #Get the appropriate baseline area
+        baseArea = baseAreas[idx]
+        #Get the value of that attribute in the catchment dataframe
+        currentPct = catchDF[nlcdAttrib].values[0]
+        #Convert to sq km (multiply by area, computed above)
+        currentArea = currentPct * baseArea / 100.0        
+        #Calculate new area
+        newArea = currentArea - changeArea
+        #Calculate new percentage
+        newPct = newArea / baseArea * 100.0
+        if newArea > 0:
+            print "   ...%s has decreased %2.2f pct (%s km2)" %(nlcdAttrib,(currentPct - newPct),changeArea)
+        #Update the data frame
+        dataDF.loc[dataDF['GRIDCODE'] == gridCode, nlcdAttrib] = newPct
+        
+##[6] Adjust downstream values
+#Extract only watershed values from the remap table
+wsOnlyDF = lutDF.loc[lutDF.CatWs == 'WS']
+#Loop through each downstream catchment record
+for gridcode in gridCodes:
+    print "Processing catchment# {}".format(gridcode)
+    #Retrieve the record for the current gridCode
+    catchDF = dataDF[dataDF.GRIDCODE == gridcode]
 
-##---REPLACE WITH LOOP---##
-for csvFile in allCSVFiles:
-   #Get the full file name (with path)
-    csvFN = os.path.join(dataFolder,csvFile)
-      
-    #Create a dataframe of the HUC8 data from the CSV file
-    dataDF = getHUC8Data(csvFN,huc8)
+    #Get the watershed area values
+    wsArea = catchDF.WsAreaSqKm.values[0]
+    wsRiparianArea = catchDF.WsAreaSqKmRp100.values[0]
+    wsMidslopeArea = catchDF.PctAg2006Slp10Ws.values[0]
+    wsHighslopeArea = catchDF.PctAg2006Slp20Ws.values[0]
+    #Add to a list
+    baseAreas = (wsArea,wsRiparianArea,wsMidslopeArea,wsHighslopeArea)
     
-    #Skip processing if not in modify list
-    if not csvFile in modCSVFiles:
-        print "Skipping {}".format(csvFile)
-        dataFrames.append(dataDF)
-        continue
-        
-    print "Processing {}".format(csvFile)
+    #Loop through the land cover types that have changed for the current catchment
+    for nlcdType, changeValues in dataDict.items():
+        ##Process catchment wide values
+        #Extract fields mapped to the current land cover type
+        nlcdDF = wsOnlyDF.loc[(wsOnlyDF.NLCDMap==nlcdType)]
+        #Loop through retrieved attributes 
+        for nlcdAttrib in nlcdDF.Attribute.values:
+            #Determine whether it's full catchment, riparian, or mid/high slope; set the index
+            if "Rp" in nlcdAttrib: idx = 1        # Riparian
+            elif "Slp10" in nlcdAttrib: idx = 2   # Mid-slope
+            elif "Slp20" in nlcdAttrib: idx = 3   # High-slope
+            else: idx = 0                         # Catchment wide
+            #Get the appropriate change area
+            changeArea = changeValues[idx]
+            #Skip if no change has occurred
+            if changeArea == 0: continue
+            #Get the appropriate baseline area
+            baseArea = baseAreas[idx]
+            #Get the value of that attribute in the catchment dataframe
+            currentPct = catchDF[nlcdAttrib].values[0]
+            #Convert to sq km (multiply by area, computed above)
+            currentArea = currentPct * baseArea / 100.0        
+            #Calculate new area
+            newArea = currentArea - changeArea
+            #Calculate new percentage
+            newPct = newArea / baseArea * 100.0
+            #Update the data frame
+            dataDF.loc[dataDF['GRIDCODE'] == gridCode, nlcdAttrib] = newPct      
 
-    #Get the data for the gridcodes the project covers
-    gridDF = dataDF[dataDF.GRIDCODE.isin(gridCodes)]
-    areaColumn = gridDF.columns[5]
-    catArea = gridDF[areaColumn].sum()
-    
-    #Get the attributes listed for the current stream cat csv file
-    attribDF = fldsDF[fldsDF["File"] == str(csvFile)]
-    
-    #Creat a dictionary to cross reference NLCD code to attribute
-    lutDict = attribDF.set_index('NLCDMap')['Attribute'].to_dict()
-
-    #Update NLCD fields for the selected catchment
-    for nlcdCode,areaChanges in dataDict.items():
-
-        #Check that change occured for the current nlcd code
-        if nlcdCode not in lutDict.keys():
-            print "...No change in NLCD %s" %nlcdCode
-            continue
-        
-        #Get the StreamCat code corresponding to the NLCD code
-        nlcdFld = str(lutDict[nlcdCode]).strip()
-        
-        #Extract the CURRENT pct area of the catchment
-        curPct = gridDF[nlcdFld].values[0]
-        
-        #Convert to total area (sq km)
-        nlcdArea = curPct * catArea / 100
-        
-        #Determine which project area value to get
-        if "Rp100" in nlcdFld: idx = 1
-        elif "Slp10" in nlcdFld: idx = 2
-        elif "Slp20" in nlcdFld: idx = 3
-        else: idx = 0
-        #Get lost area from dictionary
-        lostNlcdArea = areaChanges[idx]
-        
-        #Deduct the lost area
-        newNlcdArea = nlcdArea - lostNlcdArea
-        #Convert back to pct
-        newPct = newNlcdArea / catArea * 100
-        print "...Changing NLCD %s from %2.1f to %2.1f pct" %(nlcdCode,curPct,newPct)
-        #Update the output dataframe
-        dataDF.loc[dataDF['GRIDCODE'] == gridCode, nlcdFld] = newPct
-
-    #Append to collection
-    dataFrames.append(dataDF)
-        
-#Merge all dfs and write to file
-outDF = pd.concat(dataFrames,axis=1)
-#Replace NaN with zeros
-outDF.to_csv(outputSWDFile,index_label="OID",index=False)
- 
+#Write out data
+dataDF.to_csv(outputSWDFile,index_label="OID",index=False)
