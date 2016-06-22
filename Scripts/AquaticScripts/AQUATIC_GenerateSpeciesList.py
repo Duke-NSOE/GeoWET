e7c6305fde48754f0f631e396052aa5980869c92
John Fay <John.Fay@duke.edu>
2016-06-22 15:04:49 +1000

    Sets to interface with ArcMap

--- Scripts/AquaticScripts/AQUATIC_GenerateSpeciesList.py
+++ Scripts/AquaticScripts/AQUATIC_GenerateSpeciesList.py
@@ -1,26 +1,39 @@
 #AQUATIC_GenerateSpeciesList.py
 #
-# Generates a multivalue list of species for a given ecoregion
+# Generates a multivalue list of species for a given ecoregion, or all if specified
+#
+# REQUIRES the Fish Indicator Species file which lists the species' scientific name
+#  along with a 1 in ecoregion columns in which the species is an indicator. Scientific
+#  names MUST match the names in the SpeciesOccurrences CSV file
+#
+# June 2016
+# John.Fay@duke.edu
 
 import sys, os
 import pandas as pd
 
-sppXLSX = sys.argv[1]#r'C:\workspace\GeoWET\Data\ToolData\Fish Indicator Species for EEP.xls'
+sppXLSX = sys.argv[1]   #r'C:\workspace\GeoWET\Data\ToolData\Fish Indicator Species for EEP.xls'
 ecoregion = sys.argv[2] #"PIEDMONT"
 
 #Read in the table as a data frame
 sppDF = pd.read_excel(sppXLSX,sheetname="IndicatorSppTable")
 
-#Select records
-outDF = sppDF.ScientificName[sppDF[ecoregion] == 1]
-print "{} species selected".format(len(outDF))
+#Create the dataframe used to grab species
+if ecoregion == "ALL":
+    #If all ecoregions are specified, return all species
+    outDF = sppDF.ScientificName
+else:
+    #Otherwise return selected records
+    outDF = sppDF.ScientificName[sppDF[ecoregion] == 1]
+    print "{} species selected".format(len(outDF))
 
-#Generate multilist
+#Generate multilist string from the selected species
 outStr = ""
 for spp in outDF.values:
     outStr += ";{}".format(spp)
 outStr = outStr
 
+#Set the output, if called from ArcMap
 try:
     arcpy.SetParameterAsText(2,outStr)
 except:
