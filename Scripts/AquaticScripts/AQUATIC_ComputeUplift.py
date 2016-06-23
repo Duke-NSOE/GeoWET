d6664495d9f120e075d7c9199ea0faae27c3a093
John Fay <John.Fay@duke.edu>
2016-06-24 09:26:23 +1000

    Outsources FC creation to UTILs script

--- Scripts/AquaticScripts/AQUATIC_ComputeUplift.py
+++ Scripts/AquaticScripts/AQUATIC_ComputeUplift.py
@@ -23,10 +23,12 @@ from AQUATIC_utils import *
 #Inputs
 AqSppXLSX = r'C:\workspace\GeoWET\Data\ToolData\AquaticSpeciesList.xlsx'
 sppModelFolder = r'C:\workspace\GeoWET\Data\SpeciesModels'
-prjName = 'ExampleProject_SWD'
+prjName = 'ExampleProject4_SWD'
+catchmentFC = r'C:\workspace\GeoWET\Data\ToolData\NC_Results.gdb\CatchmentAttributes2'
 
 #Output
-outputCSV = r'C:\workspace\GeoWET\scratch\{}.csv'.format(prjName)
+outputCSV = r'C:\workspace\GeoWET\scratch\{}_Uplift.csv'.format(prjName)
+outputFC = r'C:\workspace\GeoWET\scratch\scratch.gdb\{}_Uplift'.format(prjName)
 
 ##---PROCEDURES---
 #Get the list of species
@@ -39,10 +41,9 @@ sppDFs = []
 
 #Loop through each species
 for species in sppList:
-    msg("Processing results for {}".format(species))
+    msg("Processing {}".format(species))
 
     #Get the species current projection results corresponding to the project
-    msg("[{}] Locating project result files".format(species.upper()))
     currentFN = os.path.join(sppModelFolder,species,"{}_{}CUR.csv".format(species,prjName))
     projectFN = os.path.join(sppModelFolder,species,"{}_{}.csv".format(species,prjName))
      
@@ -55,18 +56,15 @@ for species in sppList:
     upliftFlds.append(upliftFld)
 
     #Read in the Maxent results as dataframes
-    msg("[{}] Importing current likelihood".format(species.upper()))
     cDF = pd.read_csv(currentFN, dtype={'GRIDCODE':np.long})
     cDF.rename(columns={cDF.columns[2]:curFld},inplace=True)
     cDF.drop('FEATUREID',axis=1,inplace=True)
 
-    msg("[{}] Importing project likelihood".format(species.upper()))
     pDF = pd.read_csv(projectFN, dtype={'GRIDCODE':np.long})
     pDF.rename(columns={pDF.columns[2]:prjFld},inplace=True)
     pDF.drop('FEATUREID',axis=1,inplace=True)
 
     #Join the cDF values to the pDF
-    msg("[{}] Joining result tables".format(species.upper()))
     sppDF = pd.merge(cDF,pDF)
 
     #If not the first table, drop the GRIDCODE field
@@ -79,7 +77,6 @@ for species in sppList:
         continue
 
     #Calculate uplift
-    msg("...Computing uplift")
     sppDF[upliftFld] = sppDF[prjFld] - sppDF[curFld]
     
     #Add the df to the list of dfs
@@ -91,31 +88,5 @@ allDF = pd.concat(sppDFs,axis=1)
 #Write to CSV file
 allDF.to_csv(outputCSV,index=False)
 
-sys.exit(0)
-#Extract just the relevant catchments
-msg("...Creating output file")
-msg("...Extracting {} catchment records".format(len(allDF)))
-gridCodes1 = str(allDF.GRIDCODE.values.tolist())
-gridCodes2 = gridCodes1.replace('L','')[1:-1]
-whereClause = 'GRIDCODE IN ({})'.format(gridCodes2)
-selCatch = arcpy.Select_analysis(catchmentFN,outputFC,whereClause)
-
-#Add fields
-msg("...appending attribute table")
-arcpy.AddField_management(selCatch,"Current","FLOAT",6,4)
-arcpy.AddField_management(selCatch,"Project","FLOAT",6,4)
-arcpy.AddField_management(selCatch,"Uplift","FLOAT",6,4)
-
-#Append values to selCatch
-msg("...updating values")
-with arcpy.da.UpdateCursor(selCatch,("GRIDCODE","Current","Project","Uplift")) as cur:
-    for row in cur:
-        gridCode = row[0]
-        #Get the record from the dataframe
-        vals = allDF.loc[allDF.GRIDCODE == gridCode].values[0].tolist()
-        row[1] = vals[1]
-        row[2] = vals[2]
-        row[3] = vals[3]
-        cur.updateRow(row)
-
-msg("Finished!")
+#Merge with catchment features
+JoinCSVtoCatchmentFC(outputCSV,catchmentFC,outputFC,verbose=True)
