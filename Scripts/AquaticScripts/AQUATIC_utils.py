aa6b603e4c5f1e5bc735db001b67b6161473f4d0
John Fay <John.Fay@duke.edu>
2016-06-24 09:28:05 +1000

    Adds JoinCSVtoCatchmentFC function & np/pd import

--- Scripts/AquaticScripts/AQUATIC_utils.py
+++ Scripts/AquaticScripts/AQUATIC_utils.py
@@ -5,6 +5,22 @@
 # June 2016
 # John.Fay@duke.edu
 
+import sys, os
+
+#Import pandas
+try:
+    import pandas as pd
+except:
+    "Pandas is required for this module"
+    sys.exit()
+
+#Import numpy
+try:
+    import numpy as np
+except:
+    "Numpy is required for this module"
+    sys.exit()
+
 def msg(txt,severity=""):
     '''Provides messaging to interactive window or to ArcMap if run as script tool'''
     #Print the message to the interactive window
@@ -48,4 +64,60 @@ def getSpeciesList(sppFile,ecoregion):
         
     return outStr
 
+def JoinCSVtoCatchmentFC(csvFilename,catchmentFC,outFC,linkFld="GRIDCODE",verbose=False):
+    '''Selects catchment records and joins values in the CSV to them'''
+    #Import modules
+    import arcpy
+    
+    #Generate a list of gridCodes from the csvFilename
+    df = pd.read_csv(csvFilename)
+
+    #Check that linkfield occurs in the file
+    if not linkFld in df.columns:
+        msg("ERROR:{} not found in CSV file","error")
+        sys.exit(1)
+
+    #Get a unique list of link values
+    vals = pd.unique(df[linkFld])
+    valList = list(vals)            #Convert array to list
+    strList = str(valList)[1:-1]    #Convert list to string; strip '[' and ']'
+
+    #Create the where clause
+    whereClause = "{} IN ({})".format(linkFld,strList)
+
+    #Subset Catchment records
+    if verbose: msg("Extracting {} records".format(len(valList)))
+    arcpy.Select_analysis(catchmentFC,outFC,whereClause)
+
+    #Make a list of columns; ensure that the link field is first
+    cols = list(df.columns)
+    cols.remove(linkFld)    #Remove the link field
+    cols.insert(0,linkFld)  #Re-insert it as position 0
+
+    #Append CSV columns, except the link fld
+    for col in cols[1:]:
+        #Determine the column type
+        dtype = df[col].dtype
+        if dtype == np.float64:
+            if verbose: msg("...adding {} as FLOAT".format(col))
+            arcpy.AddField_management(outFC,col,"FLOAT",8,5)
+        elif dtype == np.int64:
+            if verbose: msg("...adding {} as LONG".format(col))
+            arcpy.AddField_management(outFC,col,"LONG",20)
+        else:
+            if verbose: msg("...adding {} as TEXT".format(col))
+            arcpy.AddField_management(outFC,col,"TEXT",30)
+
+    #Insert values
+    with arcpy.da.UpdateCursor(outFC,cols) as cursor:
+        for row in cursor:
+            gridCode = row[0]
+            catchVals = df[df[linkFld] == gridCode].values.tolist()
+            #Update all values
+            for i in range(1,len(catchVals)):
+                row[i] = catcVals[i]
+            cursor.updateRow(row)
+            
+    #Return the feature class
+    return outFC
     
