26815726adfd5170a1cea9c8e1e1060efaa2e314
John Fay <John.Fay@duke.edu>
2016-06-24 11:34:23 +1000

    Completes analysis with local variables

--- Scripts/AquaticScripts/AQUATIC_ComputeUplift.py
+++ Scripts/AquaticScripts/AQUATIC_ComputeUplift.py
@@ -1,16 +1,10 @@
 #AQUATIC_ComputeUplift.py
 #
-# Isolates all species within an ecoregion and uses them to compute
-# uplift (change between current and alternate species likelihood)
-# for each species and then an average of all of them. 
+# Runs Maxent projections for the supplied projections SWD file for all
+#  species in the supplied ecoregion and then merges the uplift result into
+#  a single table, along with averages, that is joined to the catchment features.
 #
-# Requires:
-# - The AquaticSpeciesList.xlsx file (to extract species for
-#   a given ecoregion).
-# - The species folder, containing completed runs for all species
-# - A project name, e.g. "ExampleProject2_SWD" that has been run as a
-#   projection in all MaxEnt species models.
-# - The AQUATICS_util script functions
+# The output must be a feature class in a geodatabase to preserve column names.
 #
 # June 2016
 # John.Fay@duke.edu
@@ -18,51 +12,64 @@
 import sys, os
 import pandas as pd
 import numpy as np
-from AQUATIC_utils import *
+import AQUATIC_utils as aq
 
 #Inputs
+prjSWDFile = r'C:\workspace\GeoWET\Data\Projects\ExampleProject4_SWD.csv'
+ecoregion = "PIEDMONT"
 AqSppXLSX = r'C:\workspace\GeoWET\Data\ToolData\AquaticSpeciesList.xlsx'
 sppModelFolder = r'C:\workspace\GeoWET\Data\SpeciesModels'
-prjName = 'ExampleProject4_SWD'
 catchmentFC = r'C:\workspace\GeoWET\Data\ToolData\NC_Results.gdb\CatchmentAttributes2'
 
+#Derived inputs
+prjName = os.path.basename(prjSWDFile[:-4])
+prjFolder = os.path.dirname(prjSWDFile)
+curSWDFile = prjSWDFile[:-4] + 'CUR.csv'
+
 #Output
-outputCSV = r'C:\workspace\GeoWET\scratch\{}_Uplift.csv'.format(prjName)
+outputCSV = os.path.join(prjFolder,'{}_Uplift.csv'.format(prjName))
+tmpFile = os.path.join(prjFolder,'tmp_{}.csv'.format(prjName))
 outputFC = r'C:\workspace\GeoWET\scratch\scratch.gdb\{}_Uplift'.format(prjName)
 
 ##---PROCEDURES---
 #Get the list of species
-sppMultiString = getSpeciesList(AqSppXLSX,"PIEDMONT")
+sppMultiString = aq.getSpeciesList(AqSppXLSX,"PIEDMONT")
 sppList = sppMultiString.split(";")
 
-#running list of uplift flds
-upliftFlds = []
+#Create a list of species dataframes to merge
 sppDFs = []
 
-#Loop through each species
+#Create a list of uplift field columns, for computing averages
+upliftFlds = []
+
+#Loop through each species, compute uplift and current tables
 for species in sppList:
-    msg("Processing {}".format(species))
-
-    #Get the species current projection results corresponding to the project
-    currentFN = os.path.join(sppModelFolder,species,"{}_{}CUR.csv".format(species,prjName))
-    projectFN = os.path.join(sppModelFolder,species,"{}_{}.csv".format(species,prjName))
-     
-    #Generate a brief species name for columns
+    aq.msg("Analyzing {}".format(species))
+    
+    #Create a short name for the spp
     genus,spp = species.split("_")
-    sppName = "{}_{}".format(genus[1],spp)
-    curFld = "{}_C".format(sppName)
-    prjFld = "{}_P".format(sppName)
-    upliftFld = "{}_U".format(sppName)
+    sppName = "{}{}".format(genus[0],spp[:5])
+
+    #Create field names
+    curFld ='C_{}'.format(sppName)
+    prjFld ='P_{}'.format(sppName)
+    upliftFld = 'U_{}'.format(sppName)
     upliftFlds.append(upliftFld)
-
-    #Read in the Maxent results as dataframes
-    cDF = pd.read_csv(currentFN, dtype={'GRIDCODE':np.long})
-    cDF.rename(columns={cDF.columns[2]:curFld},inplace=True)
+    
+    #Get the lamda file
+    lamdaFile = os.path.join(sppModelFolder,species,"{}.lambdas".format(species))
+                            
+    #Compute current conditions for catchments; convert to dataframe
+    aq.maxentProject(lamdaFile,curSWDFile,tmpFile,verbose=False)
+    cDF = pd.read_csv(tmpFile,dtype={'GRIDCODE':np.long})
     cDF.drop('FEATUREID',axis=1,inplace=True)
-
-    pDF = pd.read_csv(projectFN, dtype={'GRIDCODE':np.long})
-    pDF.rename(columns={pDF.columns[2]:prjFld},inplace=True)
+    cDF.rename(columns={'{} logistic values'.format(species):curFld},inplace=True)
+    
+    #Compute the project uplift; convert to dataframe
+    aq.maxentProject(lamdaFile,prjSWDFile,tmpFile,verbose=False)
+    pDF = pd.read_csv(tmpFile,dtype={'GRIDCODE':np.long})
     pDF.drop('FEATUREID',axis=1,inplace=True)
+    pDF.rename(columns={'{} logistic values'.format(species):prjFld},inplace=True)
 
     #Join the cDF values to the pDF
     sppDF = pd.merge(cDF,pDF)
@@ -73,11 +80,15 @@ for species in sppList:
 
     #Ensure there's an intersection
     if len(sppDF) == 0:
-        msg("ERROR: {} was not modeled for the projection area".format(species),"error")
+        aq.msg("ERROR: {} was not modeled for the projection area".format(species),"error")
         continue
 
     #Calculate uplift
     sppDF[upliftFld] = sppDF[prjFld] - sppDF[curFld]
+
+    #Drop cur and uplift fields
+    sppDF.drop(prjFld,axis=1,inplace=True)
+    sppDF.drop(curFld,axis=1,inplace=True)
     
     #Add the df to the list of dfs
     sppDFs.append(sppDF)
@@ -85,8 +96,15 @@ for species in sppList:
 #Merge all dfs
 allDF = pd.concat(sppDFs,axis=1)
 
+#Compute average uplift
+allDF["AvgUplift"] = allDF[upliftFlds].mean(axis=1)
+
 #Write to CSV file
 allDF.to_csv(outputCSV,index=False)
 
+#Delete tmpFile
+os.remove(tmpFile)
+
 #Merge with catchment features
-JoinCSVtoCatchmentFC(outputCSV,catchmentFC,outputFC,verbose=True)
+aq.msg("Joining uplift table to {}".format(outputFC))
+aq.JoinCSVtoCatchmentFC(outputCSV,catchmentFC,outputFC,verbose=True)
